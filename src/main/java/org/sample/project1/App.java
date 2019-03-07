b0d84eae9cb2e4717815d8e1460cfec840c08297
Suresh <suresh.avvaru123@gmail.com>
2019-03-07 09:12:10 +0530

    Updated on 7th march

--- src/main/java/org/sample/project1/App.java
+++ src/main/java/org/sample/project1/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Working on new application and it is build on maven" );
+        System.out.println( "Deploying in Apache Tomcat Server 8.5.38v" );
     }
 }
